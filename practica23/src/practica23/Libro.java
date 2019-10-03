d0b4edc301aa90d9cad7af13ed682a70c6602011
ghernandez <ghernandez@PROF-TC2.labs.cbtis94.local>
2019-10-03 11:49:17 -0500

    Se agregó un constructor con parámetros y se creo el objeto libro1

--- practica23/src/practica23/Libro.java
+++ practica23/src/practica23/Libro.java
@@ -15,4 +15,50 @@ public class Libro {
      * Constructor vacío.
      */
     public Libro(){}
+
+    public Libro(String Autor, String Titulo, String ISBN, int Paginas) {
+        this.Autor = Autor;
+        this.Titulo = Titulo;
+        this.ISBN = ISBN;
+        this.Paginas = Paginas;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getPaginas() {
+        return Paginas;
+    }
+
+    public void setPaginas(int Paginas) {
+        this.Paginas = Paginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "Autor=" + Autor + ", Titulo=" + Titulo + ", ISBN=" + ISBN + ", Paginas=" + Paginas + '}';
+    }
+    
+    
 }
--- practica23/src/practica23/Practica23.java
+++ practica23/src/practica23/Practica23.java
@@ -2,6 +2,8 @@ package practica23;
 
 public class Practica23 {
     public static void main(String[] args) {
+        Libro libro1 = new Libro();
         
+        System.out.println(libro1.toString());
     }
 }
