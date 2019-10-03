e472d759ac429ac2e1b0eae8eb65696f4adc0e1c
ghernandez <ghernandez@PROF-TC2.labs.cbtis94.local>
2019-10-03 11:37:11 -0500

    Código base del proyecto, se creó la clase Libro y los atributos.

---
+++ practica23/src/practica23/Libro.java
@@ -0,0 +1,18 @@
+package practica23;
+
+/**
+ *
+ * @author ghernandez
+ */
+public class Libro {
+    // atributos
+    private String Autor;
+    private String Titulo;
+    private String ISBN;
+    private int Paginas;
+    
+    /**
+     * Constructor vacío.
+     */
+    public Libro(){}
+}
---
+++ practica23/src/practica23/Practica23.java
@@ -0,0 +1,7 @@
+package practica23;
+
+public class Practica23 {
+    public static void main(String[] args) {
+        
+    }
+}
